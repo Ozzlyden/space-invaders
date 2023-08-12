b326f06f307b6ca5e1919c9926ca5fbb7cc912af
Victor Valente <Valente-victor@hotmail.com>
2023-08-12 02:09:32 -0300

    Update: Spawn Enemies

--- src/com/victor/main/EnemySpawn.java
+++ src/com/victor/main/EnemySpawn.java
@@ -9,22 +9,28 @@ public class EnemySpawn {
 	
 	public int targetTimeEnemy1 = 60 * 2;
 	public int targetTimeEnemy2 = 60 * 4;
-	public int curTime = 0;
+	public int curTimeEn1 = 0;
+	public int curTimeEn2 = 0;
+
 	
 	public void tick() {
-		curTime++;
+		curTimeEn1++;
+		curTimeEn2++;
 		
 		// SPAWN ENEMY1
-		if(curTime == targetTimeEnemy1) {
+		if(curTimeEn1 == targetTimeEnemy1) {
+			curTimeEn1 = 0;
+
 			int xx = Entity.rand.nextInt(Game.WIDTH - 16);	// -16 para nao sair da tela
 			int yy = 0;
 	
 			Enemy1 enemy1 = new Enemy1(xx, yy, 16, 16, 0.5, Entity.ENEMY1);
 			Game.entities.add(enemy1);
 		}
+		
 		// SPAWN ENEMY2
-		else if(curTime == targetTimeEnemy2) {
-			curTime = 0;
+		else if(curTimeEn2 == targetTimeEnemy2) {
+			curTimeEn2 = 0;
 			int xx = Entity.rand.nextInt(Game.WIDTH - 16);	// -16 para nao sair da tela
 			int yy = 0;
 		
